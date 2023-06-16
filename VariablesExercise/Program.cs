ca1e7a2c3ebc3fad70b680eff2887fa088593480
Brandon Levinsky <blevinsky91@gmail.com>
2023-06-16 12:05:54 -0400

    variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Cosmo";
+            int dogAge = 3;
+            char dollar = '$';
+            bool crazy = true;
+            double x = 77.77;
+            decimal y = 7.7m;
+
+            Console.WriteLine($"My dog's name is {dogName} and he is {dogAge} years old!");
         }
     }
 }
+
